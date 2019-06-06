51e35239576affbac218a947d27265840c96ddbf
bradnowak <20384954+Bradnowak@users.noreply.github.com>
2019-06-05 23:12:18 -0500

    Update Elastiget.py

--- Elastiget.py
+++ Elastiget.py
@@ -2,25 +2,24 @@ from os import system, name
 import json
 from time import sleep
 
-print("Pulling down prerequisite modules...")
-system("pip3 install elasticsearch --user")
-system("pip3 install tqdm --user")
+
+#print("Pulling down prerequisite modules...")
+#system("pip3 install elasticsearch --user")
+#system("pip3 install tqdm --user")
 
 from elasticsearch import Elasticsearch
-
 
 connected = False
 #ip = '192.168.1.172'
 #port = '9200'
 
+
 def clear():
-    # for windows
     if name == 'nt':
         _ = system('cls')
-
-        # for mac and linux(here, os.name is 'posix')
     else:
         _ = system('clear')
+
 
 def testconnection(es):
     global connected
@@ -42,14 +41,32 @@ def connect():
     return conn
 
 
+def process_hits(hits):
+    hits_list = []
+    for item in hits:
+        hits_list.append((json.dumps(item, indent=2)))
+    return hits
+
 def search(es, index_name, topic, query):
-    if topic == None and query == None:
+    hits = []
+    if topic is None and query is None:
         search_q = '{ "size": 10000, "query": { "match_all": {}}}'
-        res = es.search(index=index_name, body=search_q)
     else:
         search_q = '{ "size": 10000, "query": { "match": { "' + topic + '": { "query": "' + query + '" } } } }'
-        res = es.search(index=index_name, body=search_q)
-    return res
+
+    res = es.search(index=index_name, body=search_q, scroll='5m')
+    sid = res['_scroll_id']
+    scroll_size = res['hits']['total']
+    print("Scroll size: " + str(scroll_size))
+
+    while scroll_size > 0:
+        print('Scrolling...')
+        res = es.scroll(scroll_id=sid, scroll='5m')
+        sid = res['_scroll_id']
+        scroll_size = len(res['hits']['hits'])
+        print("Scroll size: " + str(scroll_size))
+        hits.append(process_hits(res['hits']['hits']))
+    return hits
 
 
 def get_index(es):
@@ -124,23 +141,46 @@ def main_menu(es):
     return _topic, _q_string, _index
 
 
-def save():
-    ans = input("Save file to disk? ")
-    if ans== "yes" or ans =="y":
-        f_name = input("Input file name: ")
-        if ".json" in f_name:
-            f = open(f_name, "w")
-            f.write(text)
+def save(output):
+    ans = input("Save to disk? ")
+    if ans == "yes" or ans == "y":
+        ans2 = input("[0] Single file or [1] Multiple files?")
+        while ans2 != '0' and ans2 != '1':
+            ans2 = input("Invalid answer.  [0] Single file or [1] Multiple files?")
+
+        if ans2 == '0':
+            f_name = input("Input file name: ")
+
+            if ".json" in f_name:
+                f = open(f_name, "a+")
+            else:
+                f = open(f_name + ".json", "a+")
+
+            for out_list in output:
+                for in_list in out_list:
+                    f.write(json.dumps(in_list['_source'], indent=4))
+                    f.write('\n')
             f.close()
-        else:
-            f = open(f_name + ".json", "w")
-            f.write(text)
-            f.close()
+
+        elif ans2 == '1':
+            f_name = input("Input base file name: ")
+            i = 1
+
+            for out_list in output:
+                for in_list in out_list:
+                    f = open(f_name + str(i) + '.json', 'w+')
+                    f.write(json.dumps(in_list['_source'], indent=4))
+                    i += 1
+                    f.close()
+
+
+
     elif ans == "no" or ans == "n":
         pass
     else:
         print("Please type yes or no.")
-        save()
+        save(output)
+
 
 
 if __name__ == "__main__":
@@ -151,9 +191,7 @@ if __name__ == "__main__":
     while connected:
         print("\n" + "*" * 10 + "Connected to elastic cluster at: " + ip + " via port: " + port + "*" * 10 + "\n")
         topic, q_string, index = main_menu(es_obj)
-        query = search(es_obj, index, topic, q_string)
-        text = (json.dumps(query, indent=4, sort_keys=True))
-        print(text)
-        save()
+        hits = search(es_obj, index, topic, q_string)
+        save(hits)
 
     print("Disconnected from elastic cluster")
